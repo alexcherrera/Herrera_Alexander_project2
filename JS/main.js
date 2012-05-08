6654da173344b02873827be8927e741869928350
Alexander Herrera <alexcherrera@Alexanders-MacBook-Pro.local>
2012-05-08 00:03:14 -0400

    Correcting errors

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,9 @@ Project 2
 */
 
 window.addEventListener("DOMContentLoaded", function(){
+
 //Getting the elements by id
-	function getId(e) {
+	function idTag(e) {
 		var elementId = document.getElementById(e);
 		return elementId;
 
@@ -13,15 +14,14 @@ window.addEventListener("DOMContentLoaded", function(){
 
 
 //Getting the element by tag name
-	function getTagName (t) {
+	function tagName (t) {
 		var tagName = document.getElementsByTagName(t);
 		return tagName;
 	}
 //Variables
 
 
-//Get all data by id & tags
-	function  formInformation() {
+/*
 		var chooseMajorId = getId("MajorByDepartmentSchools");
 		var choiceOfArchTag = getTagName("Architecture");
 		var choiceOfBioTag = getTagName("Biology");
@@ -38,64 +38,91 @@ window.addEventListener("DOMContentLoaded", function(){
 		var emailId = getId("Email");
 		var personId = getId("Person");
 		var noteSectionId = getId("NoteSection");
-	}
-formInformation();
-
-//Storage:
-function saveInformation () {
-	var id = Math.floor(Math.random() * 10000023);
-	//Get all values from the form:
-	var storeJSON = {
-			"majorId": 
-				[
-					"MajorByDepartmentSchools", "chooseMajorId".value
-				],
-			"courseNameId":
-				[
-					"CourseName", "courseNameId".value
-				],
-			"CourseSectionId":
-				[
-					"CourseSection", "courseSelectionId".value
-				],
-			"TopicAndSectionId":
-				[
-					"TopicAndSection", "topicAndSectionId".value	
-				],
-			"todaysDateId":
-				[
-					"todaysDateId", "todaysDateId".value
-				],
-			"dueDateId":
-				[
-					"dueDateId", "dueDateId".value
-				],
-			"emailId":
-				[
-					"emailId", "emailId".value
-				],
-			"personId":
-				[
-					"personId", "personId".value
-				],
-			"noteSectionId":
-				[
-					"noteSectionId", "noteSectionId".value
-				],
+*/
+//Radio function
+function getRadioInput () {
+	var savedRadio = document.forms[0].turnin;
+	for(var r = 0; r < savedRadio.length; r++) {
+		if(savedRadio[r].checked){
+			radioChoice = savedRadio[r].value;
 		}
-	localStorage.setItem (id, JSON.stringify(storeJSON));
-	alert("Assignments Saved!");
+	}
 }
 
 
+
+function saveInformation () {
+	var id = Math.floor(Math.random() * 10000023);
+	getRadioInput();
+
+	//Get all values from the form:
+	var store = {};
+		store.major = ["Major By Department Schools", idTag("MajorByDepartmentSchools").value];
+		store.courseName = ["Course Name", idTag("CourseName").value];
+		store.courseSection = ["Course Section", idTag("CourseSection").value];
+		store.topicAndSection = ["Topic and Section", idTag("TopicAndSection").value];
+		store.todaysDate = ["Todays Date", idTag("TodaysDate").value];
+		store.dueDate = ["Due Date ", idTag("DueDate").value];
+		store.email = ["Email", idTag("Email").value];
+		store.person = ["Person", idTag("Person").value];
+		localStorage.setItem(id, JSON.stringify(store));
+		alert("Assignments Saved!");
+}
+
+var radioChoice;
+/*
+	var storeJSON = {
+			"chooseMajorId": 
+				[
+					"MajorByDepartmentSchools", "chooseMajorId.value"
+				],
+			"courseNameId":
+				[
+					"CourseName", "courseNameId.value"
+				],
+			"courseSectionId":
+				[
+					"CourseSection", "courseSelectionId.value"
+				],
+			"topicAndSectionId":
+				[
+					"TopicAndSection", "topicAndSectionId.value"	
+				],
+			"todaysDateId":
+				[
+					"todaysDate", "todaysDateId.value"
+				],
+			"dueDateId":
+				[
+					"dueDate", "dueDateId.value"
+				],
+			"emailId":
+				[
+					"email", "emailId.value"
+				],
+			"personId":
+				[
+					"person", "personId.value"
+				],
+			"noteSectionId":
+				[
+					"noteSection", "noteSectionId.value"
+				]
+		}
+	localStorage.setItem(id, JSON.stringify(storeJSON));
+	alert("Assignments Saved!");
+}
+*/
+
 saveInformation();
+
 
 //Creating links and Submit events
 //var linkOfClear = getId("clear");
 //linkOfClear.addEventListener("click", eraseInformation);
 //var linkOfDisplay = getId("display");
 //linkOfDisplay.addEventListener("click", retrieveInformation);
-var linkOfSave = getId("submit");
+var linkOfSave = idTag("submit");
 linkOfSave.addEventListener("click", saveInformation);
 
 
