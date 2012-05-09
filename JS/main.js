417fab0eacb3af6d76420887c5b77c7767c7ea89
Alexander Herrera <alexcherrera@Alexanders-MacBook-Pro.local>
2012-05-09 13:53:57 -0400

    push

--- JS/main.js
+++ JS/main.js
@@ -22,16 +22,23 @@ window.addEventListener("DOMContentLoaded", function(){
 
 
 function saveInformation(){
-		var id                = Math.floor(Math.random()*100000001);
-		
-		//localStorage.setItem('id', JSON.info);
+			var id                = Math.floor(Math.random()*100000001);
+		getSelectionRadio();
+		var info              = {};
+			info.major        = ["Major Choice", idTag("majorChoice").value];
+			info.cName        = ["Course Name", idTag("courseName").value];
+			info.cSection     = ["Course Section", idTag('courseSection').value];
+			info.topicAndSec  = ["Topic and Section", idTag('topicAndSection').value];
+			info.todaysDate   = ["Today's Date", idTag('todaysDate').value];
+			info.dueDate      = ["Due Date", idTag('dueDate').value];
+			info.option       = ["Turn In Option", optionValue];
+			info.note         = ["Note Section", idTag('add').value];
+		localStorage.setItem(id, JSON.stringify(info));
 		alert("Assignment Saved!!");
-		
+		console.log(id);
+}
 //var getStorage = localStorage.getItem(id, 'info');
 //console.log('id', JSON.parse(info));
-}
-var choose = idTag("majorChoice").document.getElementsByTagName('Architecture');
-console.log(choose);
 
 //var getStorage = localStorage.getItem(id, 'info');
 var save = idTag('submit');
