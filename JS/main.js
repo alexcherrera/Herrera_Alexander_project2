c624faa2e05bda881ab773acb0cbd756f18e445b
Alexander Herrera <alexcherrera@unknown68a86d324b66.gateway.2wire.net>
2012-05-09 16:44:59 -0400

    changed

--- JS/main.js
+++ JS/main.js
@@ -4,48 +4,44 @@ Project 2
 */
 
 window.addEventListener("DOMContentLoaded", function(){
-	console.log("working");
-	//alert(localStorage.key(1));
+    console.log("working");
+    //alert(localStorage.key(1));
 //Getting the elements by id
-	function idTag(e){
-		var tagId = document.getElementById(e);
-		return tagId;
-	}
+    function idTag(e){
+        var tagId = document.getElementById(e);
+        return tagId;
+    }
 
-	function getSelectionRadio(){
-		var buttonRadio = idTag('collegeForm').turnin;
-		for(var i = 0; i < buttonRadio.length; i++){
-			if(buttonRadio[i].checked){
-				optionValue = buttonRadio[i].value;
-			}
-		}
-	}
-
-
-function saveInformation(){
-			var id                = Math.floor(Math.random()*100000001);
-		
-		getSelectionRadio();
-		var info              = {};
-			info.major        = ["Major Choice", idTag("majorChoice").value];
-			info.cName        = ["Course Name", idTag("courseName").value];
-			info.cSection     = ["Course Section", idTag('courseSection').value];
-			info.topicAndSec  = ["Topic and Section", idTag('topicAndSection').value];
-			info.todaysDate   = ["Today's Date", idTag('todaysDate').value];
-			info.dueDate      = ["Due Date", idTag('dueDate').value];
-			info.option       = ["Turn In Option", optionValue];
-			info.note         = ["Note Section", idTag('add').value];
-		localStorage.setItem(id, JSON.stringify(info));
-	//	alert("Assignment Saved!!");
-		
+    function getSelectionRadio(){
+        var buttonRadio = idTag('collegeForm').turnin;
+        for(var i = 0; i < buttonRadio.length; i++){
+            if(buttonRadio[i].checked){
+               optionValue = buttonRadio[i].value; 
+             
+           }
+        }
+    }
+//var optionValue;
+function saveInformation(evt){
+            var id                = Math.floor(Math.random()*100000001);
+        
+        getSelectionRadio();
+        var info              = {};
+            info.major        = ["Major Choice", idTag("majorChoice").value];
+            info.cName        = ["Course Name", idTag("courseName").value];
+            info.cSection     = ["Course Section", idTag('courseSection').value];
+            info.topicAndSec  = ["Topic and Section", idTag('topicAndSection').value];
+            info.todaysDate   = ["Today's Date", idTag('todaysDate').value];
+            info.dueDate      = ["Due Date", idTag('dueDate').value];
+            info.option       = ["Turn In Option", optionValue];
+            info.note         = ["Note Section", idTag('add').value];
+        console.log(info);
+        localStorage.setItem(id, JSON.stringify(info));
+      //  alert("Assignment Saved!!");
+      evt.preventDefault();
 }
-
-//var getStorage = localStorage.getItem(id, 'info');
-//console.log('id', JSON.parse(info));
-
-//var getStorage = localStorage.getItem(id, 'info');
 var save = idTag('submit');
 save.addEventListener("click", saveInformation);
-
+//console.log(save);
 
 });
