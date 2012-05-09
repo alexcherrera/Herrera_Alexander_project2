e4b244b27fcac6b9dcedcd46f219e600b5ef664f
Alexander Herrera <alexcherrera@Alexanders-MacBook-Pro.local>
2012-05-09 13:45:33 -0400

    current

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,14 @@ Project 2
 */
 
 window.addEventListener("DOMContentLoaded", function(){
-
+	
 //Getting the elements by id
 	function idTag(e){
-		var tagId = document.getElementById('e');
+		var tagId = document.getElementById(e);
 		return tagId;
 	}
 
-	function getSelectionRadio() {
+	function getSelectionRadio(){
 		var buttonRadio = idTag('collegeForm').turnin;
 		for(var i = 0; i < buttonRadio.length; i++){
 			if(buttonRadio[i].checked){
@@ -19,27 +19,23 @@ window.addEventListener("DOMContentLoaded", function(){
 			}
 		}
 	}
-var store = idTag('submit');
-store.addEventListener("click", storeInformation);
-console.log(store);
 
-	function storeInformation(){
+
+function saveInformation(){
 		var id                = Math.floor(Math.random()*100000001);
-		getSelectionRadio();
-		var info              = {};
-			info.major        = ["Major Choice", idTag('majorChoice').value];
-			info.cName        = ["Course Name", idTag('courseName').value];
-			info.cSection     = ["Course Section", idTag('courseSection').value];
-			info.topicAndSec  = ["Topic and Section", idTag('topicAndSection').value];
-			info.todaysDate   = ["Today's Date", idTag('todaysDate').value];
-			info.dueDate      = ["Due Date", idTag('dueDate').value];
-			info.option       = ["Turn In Option", optionValue];
-			info.note         = ["Note Section", idTag('add').value];
-		localStorage.setItem(id, JSON.stringify(info));
+		
+		//localStorage.setItem('id', JSON.info);
 		alert("Assignment Saved!!");
-	}
+		
+//var getStorage = localStorage.getItem(id, 'info');
+//console.log('id', JSON.parse(info));
+}
+var choose = idTag("majorChoice").document.getElementsByTagName('Architecture');
+console.log(choose);
 
-
+//var getStorage = localStorage.getItem(id, 'info');
+var save = idTag('submit');
+save.addEventListener("click", saveInformation);
 
 
 });
